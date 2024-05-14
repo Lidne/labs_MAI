7a067cca8f9c093778c8b17f9a003ecc85821abc
Lidne <play.gt.play@yandex.ru>
2024-05-14 13:31:42 +0300

    made lab 24 (partially)

--- 2sem/lab24/lab24.cpp
+++ 2sem/lab24/lab24.cpp
@@ -1,338 +1,265 @@
 #include <iostream>
-#include <string>
-#include <unordered_set>
-#include <list>
 
 using namespace std;
 
 class Node {
-    Node *left;
-    Node *right;
+  Node *left;
+  Node *right;
 
-   public:
-    string d;  // original string
-    string s;  // atomic symbol of this node
+ public:
+  string d;  // original string
+  string s;  // atomic symbol of this node
 
-    Node *getLeft() {
-        return this->left;
-    }
+  Node *getLeft() { return this->left; }
 
-    Node *getRight() {
-        return this->right;
-    }
+  Node *getRight() { return this->right; }
 
-    void setLeft(Node *n) {
-        this->left = n;
-    }
+  void setLeft(Node *n) { this->left = n; }
 
-    void setRight(Node *n) {
-        this->right = n;
-    }
+  void setRight(Node *n) { this->right = n; }
 
-    void print() {
-        cout << d << endl;
-    }
+  void print() { cout << d; }
 
-    Node() {
-        this->d = "";
-        this->s = "";
-        this->left = NULL;
-        this->right = NULL;
-    }
+  Node() {
+    this->d = "";
+    this->s = "";
+    this->left = NULL;
+    this->right = NULL;
+  }
 
-    Node(string d) {
-        this->d = d;
-        this->s = "";
-        this->left = NULL;
-        this->right = NULL;
-    }
+  Node(string d) {
+    this->d = d;
+    this->s = "";
+    this->left = NULL;
+    this->right = NULL;
+  }
 
-    Node(string d, Node *left, Node *right) {
-        this->d = d;
-        this->s = "";
-        this->left = left;
-        this->right = right;
-    }
+  Node(string d, Node *left, Node *right) {
+    this->d = d;
+    this->s = "";
+    this->left = left;
+    this->right = right;
+  }
 
-    Node(Node *left, Node *right) {
-        this->d = "";
-        this->s = "";
-        this->left = left;
-        this->right = right;
-    }
+  Node(Node *left, Node *right) {
+    this->d = "";
+    this->s = "";
+    this->left = left;
+    this->right = right;
+  }
 };
 
 class GrammTree {
-    Node *root;
-    unordered_set<int> commonFactors;
+  Node *root;
 
-   public:
-    // void insert(Node *root, Node *node) {
-    //     if (root->d <= node->d) {
-    //         if (root->getRight() == nullptr) {
-    //             root->setRight(node);
-    //         } else {
-    //             this->insert(root->getRight(), node);
-    //         }
-    //     }
-    //     if (root->d > node->d) {
-    //         if (root->getLeft() == nullptr) {
-    //             root->setLeft(node);
-    //         } else {
-    //             this->insert(root->getLeft(), node);
-    //         }
-    //     }
-    // }
+ public:
+  void print(const string &prefix, Node *node, bool isLeft) {
+    if (node != nullptr) {
+      cout << prefix;
+      cout << "L__";
+      cout << node->d << endl;
 
-    // void insert(Node *node) {
-    //     if (this->root->d <= node->d) {
-    //         if (this->root->getRight() == nullptr) {
-    //             this->root->setRight(node);
-    //         } else {
-    //             this->insert(this->root->getRight(), node);
-    //         }
-    //     }
-    //     if (this->root->d > node->d) {
-    //         if (this->root->getLeft() == nullptr) {
-    //             this->root->setLeft(node);
-    //         } else {
-    //             this->insert(this->root->getLeft(), node);
-    //         }
-    //     }
-    // }
+      print(prefix + (isLeft ? "|   " : "    "), node->getLeft(), true);
+      print(prefix + (isLeft ? "|   " : "    "), node->getRight(), false);
+    }
+  }
 
-    // void insert(string d) {
-    //     Node *node = new Node(d);
-    //     if (this->root->d <= node->d) {
-    //         if (this->root->getRight() == nullptr) {
-    //             this->root->setRight(node);
-    //         } else {
-    //             this->insert(this->root->getRight(), node);
-    //         }
-    //     }
-    //     if (this->root->d > node->d) {
-    //         if (this->root->getLeft() == nullptr) {
-    //             this->root->setLeft(node);
-    //         } else {
-    //             this->insert(this->root->getLeft(), node);
-    //         }
-    //     }
-    // }
+  void print(Node *node) { print("", node, false); }
 
-    void print(const string &prefix, Node *node, bool isLeft) {
+  void print() { print("", this->root, false); }
+
+  Node *deleteNode(Node *root, string d) {
+    if (root == nullptr || (root->getLeft() == nullptr &&
+                            root->getRight() == nullptr && root->d != d))
+      return nullptr;
+
+    if (root->getLeft() == nullptr && root->getRight() != nullptr) {
+      if (root->getRight()->d == d) {
+        delete root->getRight();
+        root->setRight(nullptr);
+        root->d = "1";
+        return new Node(d);
+      }
+      return deleteNode(root->getRight(), d);
+    }
+    if (root->getLeft() != nullptr && root->getRight() == nullptr) {
+      if (root->getLeft()->d == d) {
+        delete root->getLeft();
+        root->setLeft(nullptr);
+        root->d = "1";
+        return new Node(d);
+      }
+      return deleteNode(root->getLeft(), d);
+    }
+    if (root->getLeft() != nullptr && root->getLeft()->getLeft() == nullptr &&
+        root->getLeft()->getRight() == nullptr && root->getLeft()->d == d) {
+      delete root->getLeft();
+      root->setLeft(nullptr);
+      if (root->getRight() != nullptr &&
+          root->getRight()->getLeft() == nullptr &&
+          root->getRight()->getRight() == nullptr && root->getRight()->d == d) {
+        delete root->getRight();
+        root->setRight(nullptr);
+      }
+      return new Node(d);
+    }
+
+    if (root->getRight() != nullptr && root->getRight()->getLeft() == nullptr &&
+        root->getRight()->getRight() == nullptr && root->getRight()->d == d) {
+      delete root->getRight();
+      root->setRight(nullptr);
+      if (root->getLeft() != nullptr && root->getLeft()->getLeft() == nullptr &&
+          root->getLeft()->getRight() == nullptr && root->getLeft()->d == d) {
+        delete root->getLeft();
+        root->setLeft(nullptr);
+      }
+      return new Node(d);
+    }
+
+    Node *left = deleteNode(root->getLeft(), d);
+    if (left != nullptr) return left;
+
+    Node *right = deleteNode(root->getRight(), d);
+    if (right != nullptr) return right;
+
+    return nullptr;
+  }
+
+  Node *deleteNode(string d) { return deleteNode(this->root, d); }
+
+  void symPrint(Node *node) {
+    if (node == nullptr) return;
+    symPrint(node->getLeft());
+    if (node->getLeft() == nullptr && node->getRight() == nullptr)
+      node->print();
+    symPrint(node->getRight());
+  }
+
+  void symPrint() { symPrint(this->root); }
+
+  void insertSymb(Node *node) {
+    if (node->d.length() <= 1) return;
+    Node *l = new Node(node->d.substr(0, 1));
+    Node *r = new Node(node->d.substr(1, 100));
+    node->setLeft(l);
+    insertSymb(l);
+    node->setRight(r);
+    insertSymb(r);
+  }
+
+  void parseEq() {
+    if (this->root->d.empty()) {
+      cout << "empty string" << endl;
+      return;
+    }
+    string l = "", r = "";
+    bool flag = false;
+    for (int i = 0; i < this->root->d.length(); ++i) {
+      if (this->root->d[i] == '+')
+        flag = true;
+      else if (flag)
+        r += this->root->d[i];
+      else
+        l += this->root->d[i];
+    }
+    // cout << l << ' ' << r << endl;
+    Node *left = new Node(l);
+    this->root->setLeft(left);
+    insertSymb(left);
+    Node *right = new Node(r);
+    this->root->setRight(right);
+    insertSymb(right);
+  }
+
+  string commonFactors(Node *root, string res) {
+    if (root == nullptr) return "";
+    if (root->getLeft() == nullptr && root->getRight() == nullptr) {
+      Node *node = deleteNode(this->root->getRight(), root->d);
+      // cout << node->d << endl;
+      if (node != nullptr) {
+        string s = root->d;
+        deleteNode(this->root->getLeft(), root->d);
+        return res + s;
+      }
+      return "";
+    }
+
+    return commonFactors(root->getLeft(), res) +
+           commonFactors(root->getRight(), res);
+  }
+
+  void commonFactors() {
+    if (this->root->getLeft() == nullptr && this->root->getRight() == nullptr) {
+      cout << this->root->d;
+      return;
+    }
+    bool l = (this->root->getLeft()->getLeft() == nullptr &&
+              this->root->getLeft()->getRight() == nullptr);
+    bool r = (this->root->getRight()->getLeft() == nullptr &&
+              this->root->getRight()->getRight() == nullptr);
+    string s = "";
+    if (l || r) {
+      if (this->root->getLeft()->d == this->root->getRight()->d) {
+        s = "2(" + this->root->getLeft()->d + ")\n";
+        cout << s;
+        delete this->root->getLeft();
+        delete this->root->getRight();
+        this->root->setLeft(nullptr);
+        this->root->setRight(nullptr);
+        this->root->d = s;
+        return;
+      }
+      if (l) {
+        Node *node =
+            deleteNode(this->root->getRight(), this->root->getLeft()->d);
         if (node != nullptr) {
-            cout << prefix;
-            cout << "└──";
-            cout << node->d << endl;
-
-            print(prefix + (isLeft ? "│   " : "    "), node->getLeft(), true);
-            print(prefix + (isLeft ? "│   " : "    "), node->getRight(), false);
+          s += node->d;
+          this->root->getLeft()->d = "1";
         }
-    }
-
-    void print(Node *node) {
-        print("", node, false);
-    }
-
-    void print() {
-        print("", this->root, false);
-    }
-
-    Node *find(Node *node, string d) {
-        if (node == nullptr) return nullptr;
-        if (node->d == d) return node;
-        return find((node->d > d ? node->getLeft() : node->getRight()), d);
-    }
-
-    Node *find(string d) {
-        return find(this->root, d);
-    }
-
-    Node *findPrev(Node *node, string d) {
-        if (node == nullptr) return nullptr;
-        if (node->d == d || node->getLeft() && node->getLeft()->d == d || node->getRight() && node->getRight()->d == d) return node;
-        return findPrev((node->d > d ? node->getLeft() : node->getRight()), d);
-    }
-
-    Node *findPrev(string d) {
-        return findPrev(this->root, d);
-    }
-
-    Node *getMin(Node *node) {
-        if (node == nullptr) return nullptr;
-        if (node->getLeft() == nullptr) return node;
-        return getMin(node->getLeft());
-    }
-
-    Node *getMax(Node *node) {
-        if (node == nullptr) return nullptr;
-        if (node->getRight() == nullptr) return node;
-        return getMin(node->getRight());
-    }
-
-    void deleteNode(Node *head, string d) {
-        if (head == nullptr)
-            return;
-        else if (head->d > d)
-            deleteNode(head->getLeft(), d);
-        else if (head->d < d)
-            deleteNode(head->getRight(), d);
-        else {
-            if (head->getLeft() == nullptr && head->getRight() == nullptr) {
-                Node *prev = this->findPrev(head->d);
-                if (prev->getRight() == head) {
-                    prev->setRight(nullptr);
-                }
-                if (prev->getLeft() == head) {
-                    prev->setLeft(nullptr);
-                }
-                delete head;
-                head = nullptr;
-            } else if (head->getLeft() == nullptr || head->getRight() == nullptr) {
-                Node *child;
-                if (head->getLeft() == nullptr) {
-                    child = head->getRight();
-                    head->setRight(nullptr);
-                } else {
-                    child = head->getLeft();
-                    head->setLeft(nullptr);
-                }
-                *head = *child;
-                delete child;
-                child = nullptr;
-            } else {
-                Node *leftMin = getMax(head->getLeft());
-                string tmp = leftMin->d;
-                deleteNode(head->getLeft(), leftMin->d);
-                head->d = tmp;
-            }
+      }
+      if (r) {
+        Node *node =
+            deleteNode(this->root->getLeft(), this->root->getRight()->d);
+        if (node != nullptr) {
+          s += node->d;
+          this->root->getRight()->d = "1";
         }
-        // cout << "deleted" << endl;
-    }
+      }
 
-    void deleteNode(string d) {
-        deleteNode(this->root, d);
-    }
+    } else
+      s = commonFactors(this->root->getLeft(), "");
+    cout << "result: " << s << "(";
+    symPrint(this->root->getLeft());
+    cout << " + ";
+    symPrint(this->root->getRight());
+    cout << ")\n";
 
-    void symPrint(Node *node) {
-        if (node == nullptr) return;
-        symPrint(node->getLeft());
-        node->print();
-        symPrint(node->getRight());
-    }
+    // string b = commonFactors(this->root->getRight(), "");
+    // cout << "result: " << b << "(";
+    // symPrint(this->root->getLeft());
+    // cout << " + ";
+    // symPrint(this->root->getRight());
+    // cout << ")\n";
+  }
 
-    void symPrint() {
-        symPrint(this->root);
-    }
+  GrammTree(Node *root) {
+    this->root = root;
+    parseEq();
+  }
 
-    void insertSymb(Node *node) {
-        // cout << node->d << endl;
-        if (node->d.length() <= 1) return;
-        Node *l = new Node(node->d.substr(0, 1));
-        Node *r = new Node(node->d.substr(1, 100));
-        node->setLeft(l);
-        insertSymb(l);
-        node->setRight(r);
-        insertSymb(r);
-        // cout << node->getLeft() << " " << node->getRight() << endl;
-    }
-
-    unordered_set<string> findCommonFactors(Node *root) {
-        unordered_set<string> commonFactors;
-        if (root->getLeft() == nullptr && root->getRight() == nullptr) {
-            commonFactors.insert(root->d);
-            return commonFactors;
-        }
-
-        unordered_set<string> leftFactors = findCommonFactors(root->getLeft());
-        unordered_set<string> rightFactors = findCommonFactors(root->getRight());
-
-        for (string factor : leftFactors) {
-            if (rightFactors.count(factor)) {
-                commonFactors.insert(factor);
-            }
-            cout << factor << " ";
-        }
-        cout << endl;
-
-        return commonFactors;
-    }
-
-    unordered_set<string> findCommonFactors() {
-        return findCommonFactors(this->root);
-    }
-
-    void parseEq() {
-        if (this->root->d.empty()) {
-            cout << "empty string" << endl;
-            return;
-        }
-        string l = "", r = "";
-        bool flag = false;
-        for (int i = 0; i < this->root->d.length(); ++i) {
-            if (this->root->d[i] == '+')
-                flag = true;
-            else if (flag)
-                r += this->root->d[i];
-            else
-                l += this->root->d[i];
-        }
-        // cout << l << ' ' << r << endl;
-        Node *left = new Node(l);
-        this->root->setLeft(left);
-        insertSymb(left);
-        Node *right = new Node(r);
-        this->root->setRight(right);
-        insertSymb(right);
-    }
-
-    GrammTree(Node *root) {
-        this->root = root;
-        parseEq();
-    }
-
-    GrammTree(string d) {
-        Node *rt = new Node(d);
-        this->root = rt;
-        parseEq();
-    }
+  GrammTree(string d) {
+    Node *rt = new Node(d);
+    this->root = rt;
+    parseEq();
+  }
 };
 
 int main() {
-    // GrammTree *tree = new GrammTree("2ax+7ay");
-    // tree->print();
-    // unordered_set<string> res = tree->findCommonFactors();
-    // for (string item : res) {
-    //     cout << "main " << item << ' ';
-    // }
-    // cout << endl;
-    // cout << "end\n";
-    string eq;
-    cin >> eq;
-    cout << "sd" << endl;
-    unordered_set<char> left;
-    unordered_set<char> right;
-    bool flag = false;
-    for (int i = 0; i<eq.length(); ++i) {
-        if (eq[i] == '+') flag = true;
-        else if (flag) right.insert(eq[i]);
-        else left.insert(eq[i]);
-    }
-
-    unordered_set<char> commonFactors;
-    for (char item : left) {
-        if (right.count(item)) {
-            commonFactors.insert(item);
-            right.erase(item);
-            left.erase(item);
-        }
-    }
-    for (char it : left) cout << it << ' ';
-    cout << endl;
-
-    for (char it : right) cout << it << ' ';
-    cout << endl;
-
-    for (char it : commonFactors) cout << it << ' ';
-    cout << endl;
+  string inp = "";
+  cin >> inp;
+  GrammTree *tree = new GrammTree(inp);
+  // GrammTree *tree = new GrammTree("2xx+x");
+  tree->print();
+  tree->commonFactors();
+  tree->print();
 }
